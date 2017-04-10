7dec58c8e8965934c8d6a604f935536b86ef6a54
rahulragunathan <rahul.ragunathan@gmail.com>
2017-04-09 21:05:48 -0400

    Fixed display

--- js/county-details.js
+++ js/county-details.js
@@ -1,17 +1,18 @@
 var countyData_filePath = "data/county.json";
 
 var selectedCounty = "26065";
+var yScaleFactor = 1.1;
 
 // Surface - Groundwater chart
 // Define SVG chart size
 var sg_chartMargin = {
-    top: 5,
-    right: 10,
-    bottom: 5,
-    left: 10
+  top: 20,
+  right: 20,
+  bottom: 30,
+  left: 40
 };
-sg_chartWidth = 400 - sg_chartMargin.left - sg_chartMargin.right;
-sg_chartHeight = 400 - sg_chartMargin.top - sg_chartMargin.bottom;
+sg_chartWidth = 200 - sg_chartMargin.left - sg_chartMargin.right;
+sg_chartHeight = 200 - sg_chartMargin.top - sg_chartMargin.bottom;
 
 var sg_svg, sg_xScale, sg_yScale, sg_xAxis, sg_xAxisSel, sg_yAxis, sg_yAxisSel;
 
@@ -25,7 +26,6 @@ sg_svg = d3.select("body")
         "translate(" + sg_chartMargin.left + "," + sg_chartMargin.top + ")");
 
 // define the axes
-// x
 sg_xScale = d3.scale.ordinal().rangeRoundBands([0, sg_chartWidth], 0.05);
 sg_xAxis = d3.svg.axis()
     .scale(sg_xScale)
@@ -46,12 +46,20 @@ sg_yAxisSel = sg_svg.append("g").attr("class", "y axis");
 d3.json(countyData_filePath, function(error, data) {
 
     sg_sourceData = data[selectedCounty].water_source;
+    fs_sourceData = data[selectedCounty].water_type;
+    ind_sourceData = data[selectedCounty].industry;
+
+    console.log(sg_sourceData);
+    console.log(fs_sourceData);
+    console.log(ind_sourceData);
 
     // Determine domains for axes
     sg_xScale.domain(d3.range(sg_sourceData.length));
-    sg_yScale.domain([0, d3.max(sg_sourceData, function(d) { return d.value; })]);
+    sg_yScale.domain([0, yScaleFactor * d3.max(sg_sourceData, function(d) {
+        return d.value;
+    })]);
 
-    // Add bars to graph
+    // Add bars to graphs
     sg_svg.selectAll("rect")
         .data(sg_sourceData)
         .enter()
@@ -60,11 +68,12 @@ d3.json(countyData_filePath, function(error, data) {
             return sg_xScale(i);
         })
         .attr("y", function(d) {
-            console.log(d.value);
+            console.log(sg_yScale(d.value));
             return sg_chartHeight - sg_yScale(d.value); //Height minus data value
         })
         .attr("width", sg_xScale.rangeBand())
         .attr("height", function(d) {
+            console.log(sg_yScale(d.value));
             return sg_yScale(d.value);
         });
 
